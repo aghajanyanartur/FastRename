9888aa068860634dbb2a480fae30edc0f27e5e56
Artur Aghajanyan <aghajanyan.aa1@gmail.com>
2023-04-02 09:42:36 +0400

    Initial commit

---
+++ src/main/java/art/pro/FastRename/FastRenameApplication.java
@@ -0,0 +1,13 @@
+package art.pro.FastRename;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastRenameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastRenameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/art/pro/FastRename/FastRenameApplicationTests.java
@@ -0,0 +1,13 @@
+package art.pro.FastRename;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastRenameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

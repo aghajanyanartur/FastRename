7fc6db6c48df6aca0d9a5f4b6d75c4e9534d2af1
Artur Aghajanyan <aghajanyan.aa1@gmail.com>
2023-04-02 20:34:35 +0400

    Refactored

--- src/main/java/art/pro/FastRename/FastRenameApplication.java
+++ src/main/java/art/pro/FastRename/FastRenameApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FastRenameApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FastRenameApplication.class, args);
 	}
